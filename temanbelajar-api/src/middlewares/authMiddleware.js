959e7620bb344533a7ab599754cd5df57b9b3ae1
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2026-02-12 11:42:42 +0700

    add middleware to protect routes with jwt

---
+++ temanbelajar-api/src/middlewares/authMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token)
+    return res.status(401).json({ message: "Access token is missing" });
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(403).json({ message: "Invalid access token" });
+  }
+};
+module.exports = verifyToken;
